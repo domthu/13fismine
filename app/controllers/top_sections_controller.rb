d26882fa920ed835351e2c122ec7c0a5c3898bc7
THUAL Dominique <dom_thual@yahoo.fr>
2012-06-03 20:21:52 +0200

    sample of sort and paging integration

--- app/controllers/top_sections_controller.rb
+++ app/controllers/top_sections_controller.rb
@@ -3,13 +3,61 @@ class TopSectionsController < ApplicationController
 
   before_filter :require_admin
 
+  helper :sort
+  include SortHelper
+
+#     sort_init 'last_name'
+#     sort_update %w(first_name last_name)
+#                'activity' => 'activity_id',
+#                'project' => "#{Project.table_name}.name",
+#     @contact_pages, @items = paginate :contacts,
+#       :order_by => sort_clause,
+#       :per_page => 10
+#   end
+#
+# View (table header in list.rhtml):
+#
+#   <thead>
+#     <tr>
+#       <%= sort_header_tag('id', :title => 'Sort by contact ID') %>
+#       <%= sort_header_tag('last_name', :caption => 'Name') %>
+#       <%= sort_header_tag('phone') %>
+#       <%= sort_header_tag('address', :width => 200) %>
+
   # GET /top_sections
   # GET /top_sections.xml
   def index
-    @top_sections = TopSection.all
+    #@top_sections = TopSection.all
+
+    #Sorting
+    sort_init 'sezione_top'
+    sort_update 'sezione_top' => 'sezione_top',
+                'ordinamento' => 'ordinamento',
+                'style' => 'ordinamento',
+                'se_visible' => 'se_visible'
+
+
+#    @top_sections = TopSection.all
+#    @top_section_pages, @items = paginate :top_sections,
+#           :order_by => sort_clause,
+#           :per_page => 2
 
     respond_to do |format|
-      format.html # index.html.erb
+      #ovverride for paging format.html # index.html.erb
+      format.html {
+        # Paginate results
+        #@top_section_count = TimeEntry.visible.count(:include => [:project, :issue], :conditions => cond.conditions)
+        @top_section_count = TopSection.all.count
+        @top_section_pages = Paginator.new self, @top_section_count, per_page_option, params['page']
+        @top_sections = TopSection.find(:all,
+                                  :order => sort_clause,
+                                  :limit  =>  @top_section_pages.items_per_page,
+                                  :offset =>  @top_section_pages.current.offset)
+#                                  :conditions => cond.conditions,
+#                                  :include => [:project, :activity, :user, {:issue => :tracker}],
+
+        render :layout => !request.xhr?
+      }
       format.xml  { render :xml => @top_sections }
     end
   end
@@ -39,6 +87,10 @@ class TopSectionsController < ApplicationController
   # GET /top_sections/1/edit
   def edit
     @top_section = TopSection.find(params[:id])
+#    @board.safe_attributes = params[:board]
+#    if request.post? && @board.save
+#      redirect_to_settings_in_projects
+#    end
   end
 
   # POST /top_sections
--- app/models/top_section.rb
+++ app/models/top_section.rb
@@ -8,17 +8,17 @@ class TopSection < ActiveRecord::Base
   validates_uniqueness_of :sezione_top, :case_sensitive => false
   validates_length_of :sezione_top, :maximum => 100
 
-  #boolean
+  #integer
   validates_presence_of :ordinamento
 
-  #integer
-  validates_presence_of :se_visibile
+  #boolean
+  #validates_presence_of :se_visibile
 
   #file
-  validates_presence_of :immagine
+  #validates_presence_of :immagine
 
   #text-area? CSS? HTML area
-  validates_presence_of :style
+  #validates_presence_of :style
   validates_length_of :style, :maximum => 4000
 
 
