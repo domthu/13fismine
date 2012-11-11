8caaf6e9da16c0d4ebfd9436f504bcce452ac054
THUAL Dominique <dom_thual@yahoo.fr>
2012-11-11 20:23:50 +0100

    Rename colum style to key for table top_section

--- db/migrate/20121028221129_add_key_to_top_menus.rb
+++ db/migrate/20121028221129_add_key_to_top_menus.rb
@@ -1,9 +1,12 @@
 class AddKeyToTopMenus < ActiveRecord::Migration
   def self.up
     add_column :top_menus, :key, :string
+    rename_column :top_sections, :style, :key
+
   end
 
   def self.down
     remove_column :top_menus, :key
+    rename_column :top_sections, :key, :style
   end
 end
