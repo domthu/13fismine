2f28dbbbf295f0bd6136dd0c913a59efe61f1f87
Sandro <sandro@pecchia.info>
2012-11-29 17:59:43 +0100

    triplo menu e varie

--- config/routes.rb
+++ config/routes.rb
@@ -10,15 +10,18 @@ ActionController::Routing::Routes.draw do |map|
   # If not authorized home_url --> editorial_url
  # map.home '', :controller => 'welcome' #REDMINE HOME
   #map.home '/editoriale/home', :controller => "editorial", :action => 'home' #FRONT END
-  map.home'',  :controller => "editorial", :action => 'top_menu',  :topmenu_key =>  "fiscale" #"approfondimenti -->non c'è questa voce in TopMenu?" #FRONT END
-
-  # Named Routes for static pages.
-  map.contact '/contact', :controller => 'editorial', :action => 'contact'
-  map.about '/about', :controller => 'editorial', :action => 'about'
-  map.help '/help', :controller => 'editorial', :action => 'help'
+  map.home'',  :controller => "editorial", :action => 'home'
+  # menu sopra
+  map.chisiamo '/chi_siamo', :controller => 'editorial', :action => 'chisiamo'
+  map.servizi '/servizi', :controller => 'editorial', :action => 'servizi'
+  map.contatti '/contatti', :controller => 'editorial', :action => 'contatti'
+  map.convegni '/convegni-eventi', :controller => 'editorial', :action => 'convegni'
+  map.abbonamenti  '/abbonamenti', :controller => 'editorial', :action => 'abbonamenti'
+  map.enti  '/enti_federazioni', :controller => 'editorial', :action => 'enti'
+  map.lavora '/lavora_con_noi', :controller => 'editorial', :action => 'lavora'
   map.edizioni '/edizioni', :controller => 'editorial', :action => 'edizioni'
   map.edizione '/edizione/:id', :controller => 'editorial', :action => 'edizione'
-  #map.articoli '/articoli', :controller => 'editorial', :action => 'articoli'
+  map.articoli '/articoli', :controller => 'editorial', :action => 'articoli'
   #map.registrazione '/registrazione', :controller => 'editorial', :action => 'register'
   #map.accedi        '/accedi', :controller => 'editorial', :action => 'login'
   map.quesiti '/quesiti', :controller => 'editorial', :action => 'quesiti'
