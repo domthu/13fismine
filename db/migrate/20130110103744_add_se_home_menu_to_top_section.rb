803c6d3c5c49ad41998730b6fdf57bb426f42030
THUAL Dominique <dom_thual@yahoo.fr>
2013-01-10 16:36:52 +0100

    perfomr migration

--- db/migrate/20130110103744_add_se_home_menu_to_top_section.rb
+++ db/migrate/20130110103744_add_se_home_menu_to_top_section.rb
@@ -31,21 +31,14 @@ class AddSeHomeMenuToTopSection < ActiveRecord::Migration
     #chisiamo
     create_table :profiles do |t|
       t.integer :user_id, :null => false
-      t.string :url, :null => true
       t.text :titoli, :limit => 1000, :null => true
       t.text :curriculum, :limit => 2000, :null => true
-      t.string :allegato, :limit => 250, :null => true
+      t.string :immagine_url, :limit => 255, :null => true
+      t.string :external_url, :limit => 255, :null => true
 
       t.timestamps
     end
 
-       * User data : nome cognome email contatti
-      FK UserId
-   * url esterna verso una pagina personale
-   * foto
-   * titoli
-   * curriculum
-   * PDF allegato Document
   end
 
   def self.down
@@ -53,30 +46,3 @@ class AddSeHomeMenuToTopSection < ActiveRecord::Migration
     drop_table :reservations
   end
 end
-#2/4 QUESITO
-#  * Status
-#  * Idarticolo
-
-#  creazione Utente
-#    News va in progetto QUESITI e-quesiti
-#    prende lo Stato IN ATTESA
-#      CAMPO DA CREARE
-#  MariaCristina accede al Progetto QUESITI
-#    1/3 lo rifiuta e mette lo Stato NON ATTINENTE
-#               Scrive Causale
-#      CAMPO DA CREARE
-#    pagina news modifica add PULSANTI ACCETTA
-#    2/3 Al click Andiamo su Nuovo Articolo
-#        - imposta Issue.new con l'id della news
-#        - imposta un collaboratore
-#        - definisce Top
-#        Al salvattaggio
-#          * NEWS mette lo stato IN LAVORAZIONE
-#          * ARTICOLO Crea ed associa alla news
-
-#    3/3 sceglie l'edizione (di default )
-#        SPOSTA su un edizione in pubblicazione
-
-#NOTA: Imporrt dei dati --> rivedere la procedura di import per creare
-#    news con authorid = il cliente
-#    articolo legato alla news con authorid = il collaboratore
