735d96c7710dabf3db0ce5e2b715bd72b0bba840
THUAL Dominique <dom_thual@yahoo.fr>
2013-02-15 11:00:11 +0100

    import news

--- db/migrate/20130213222960_change_news_fields.rb
+++ db/migrate/20130213222960_change_news_fields.rb
@@ -2,9 +2,10 @@ class ChangeNewsFields < ActiveRecord::Migration
   def self.up
   rename_column :news, :causale, :reply
   change_column :news, :reply, :text
-  change_column :top_sections, :se_home_menu, :integer,  :default => 0, :limit => 4
+  change_column :top_sections, :se_home_menu, :integer,  {:default => 0, :limit => 4}
   rename_column :top_sections, :se_home_menu, :hidden_menu
-  add_column :top_menus, :se_visibile, :integer,  :default => 1, :limit => 4
+  add_column :top_menus, :se_visibile, :integer, {:default => 1, :limit => 4}
+  change_column :news, :title, :string, {:limit => 255}
    end
 
   def self.down
