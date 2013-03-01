4a568387988862dd60ec8ee163c9a7b523dc17ea
Sandro <sandro@pecchia.info>
2013-03-01 09:38:48 +0100

    cambiato nome_campo da immagine_url a use_gravatar

--- db/migrate/20130219100648_user_profiles_groups.rb
+++ db/migrate/20130219100648_user_profiles_groups.rb
@@ -6,17 +6,18 @@ class UserProfilesGroups < ActiveRecord::Migration
     add_column :user_profiles, :fs_skype, :string, :limit => 65
     add_column :user_profiles, :fs_mail, :string, :limit => 65
     add_column :user_profiles, :display_in, :integer, {:limit =>1, :default => 1, :null => false}
+    rename_column :user_profiles, :immagine_url, :use_gravatar
+    change_column :user_profiles, :use_gravatar, :boolean, {:default => 1, :null => false}
     change_column :top_sections, :hidden_menu , :integer, {:limit =>1, :default => 0, :null => false}
-  end
+   end
 
   def self.down
-
     remove_column :user_profiles, :fs_qualifica
     remove_column :user_profiles, :fs_tel
     remove_column :user_profiles, :fs_fax
     remove_column :user_profiles, :fs_skype
     remove_column :user_profiles, :fs_mail
     remove_column :user_profiles, :display_in
-
+    rename_column :user_profiles, :use_gravatar, :immagine_url
   end
 end
