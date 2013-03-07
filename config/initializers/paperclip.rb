a5dc4fa2cf0267c0f22b8e7f3048d679fb74f406
Sandro <sandro@pecchia.info>
2013-03-07 23:41:05 +0100

    style della user_profile_show

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -1,17 +1,19 @@
 require "paperclip/railtie"
 Paperclip::Railtie.insert
+=begin
 module HasAvatar
-  STYLES = {:large => ["200x200#", :png],
-            :medium => ["100x100#", :png],
-            :small => ["70x70#", :png],
-            :little => ["50x50#", :png],
-            :tiny => ["24x24#", :png]}
+  STYLES = {:large => ["200x200#", :png, :jpg],
+            :medium => ["100x100#", :png,:jpg],
+            :small => ["70x70#", :png,:jpg],
+            :little => ["50x50#", :png,:jpg],
+            :tiny => ["24x24#", :png,:jpg]}
 
   def self.included(base)
     base.extend ClassMethods
   end
 
   module ClassMethods
+
     def has_avatar
       has_attached_file :avatar,
                         PAPERCLIP_DEFAULTS.merge(
@@ -23,3 +25,5 @@ module HasAvatar
     end
   end
 end
+ActiveRecord::Base.send :include, HasAvatar
+=end
