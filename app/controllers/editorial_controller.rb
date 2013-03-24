1a7c0145dcb211a76a98731ef4c0b4ae99a7ce94
Sandro <sandro@pecchia.info>
2013-03-24 13:10:23 +0100

    revision profile_new

--- app/controllers/editorial_controller.rb
+++ app/controllers/editorial_controller.rb
@@ -440,9 +440,9 @@ class EditorialController < ApplicationController
         # if @user_profile.update_attributes(:user_id => params[:user_id], :photo => params[:photo], :display_in => params[:display_in], :fs_qualifica => params[:fs_qualifica], :fs_tel => params[:fs_tel], :fs_fax => params[:fs_fax], :use_gravatar => params[:use_gravatar], :fs_skype => params[:fs_skype], :fs_mail => params[:fs_mail], :external_url => params[:external_url], :titoli => params[:titoli], :curriculum => params[:curriculum])
         #@user_profile.photo.reprocess!
         # @user_profile.save
-        flash[:notice] = fading_flash_message("il tuo profilo è stato aggiornato." + (params[:photo].nil? ? " nullo " :"non nullo") + "/" + params[:photo].to_s + "\n u-id >" + params[:user_id] + "\n p-id >" + params[:id], 5)
+        flash[:notice] = fading_flash_message("il tuo profilo è stato aggiornato.", 5)
       else
-        flash[:notice] = 'mah ... qualcosa è andato storto!'
+        flash[:notice] = 'mmmhhh ... qualcosa è andato storto!'
       end
       redirect_to :action => 'profilo_show', :id => @user_profile
       return
@@ -451,7 +451,10 @@ class EditorialController < ApplicationController
   end
 
   def profilo_new
+    @uid= params[:id]
+    @this_user = User.find_by_id(@uid)
     render :layout => "editorial_edit"
+
   end
 
 # sandro sotto  -- per non usare i metodi di default in caso si voglia utilizzare i default in redmine
@@ -471,6 +474,7 @@ class EditorialController < ApplicationController
   end
 
 
+=begin
   def profilo_update
     @user_profile = UserProfile.find(params[:id])
     respond_to do |format|
@@ -487,7 +491,7 @@ class EditorialController < ApplicationController
       end
     end
   end
-
+=end
   def profilo_destroy
     @user_profile = UserProfile.find(params[:id])
     @user_profile.destroy
