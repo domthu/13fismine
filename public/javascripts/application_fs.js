c58d6e00435ddb53e1ec4e50ffa49f7774a73320
THUAL Dominique <dom_thual@yahoo.fr>
2013-05-17 16:08:28 +0200

    handlefileset

--- public/javascripts/application_fs.js
+++ public/javascripts/application_fs.js
@@ -1,26 +1,58 @@
 /*funzione aggiunta per il preview di paperclip */
 
-function handleFileSelect(evt) {
+// valori accettati 1= Immagine Avatar Papareclip , 0 = Immagine Gravatar , 2  = Nuova immagine upload
+function funct_avatar(val) {
+    if (val == 'g') {
+        document.getElementById('fs-photo-edit-i').style.display = 'none';
+        document.getElementById('fs-photo-edit-a').style.display = 'none';
+        document.getElementById('fs-photo-edit-g').style.display = 'block';
+    }
+    else if (val == 'a') {
+        document.getElementById('fs-photo-edit-i').style.display = 'none';
+        document.getElementById('fs-photo-edit-a').style.display = 'block';
+        document.getElementById('fs-photo-edit-g').style.display = 'none';
+    }
+    else if (val == 'i') {
+        document.getElementById('fs-photo-edit-i').style.display = 'block';
+        document.getElementById('fs-photo-edit-a').style.display = 'none';
+        document.getElementById('fs-photo-edit-g').style.display = 'none';
+        document.getElementById('use_gravatar_false').checked = true;
+        document.getElementById('use_gravatar_true').checked = false;
+        document.getElementById('use_gravatar_false').setAttribute('checked', 'checked');
+    }
+
+}
+function handleFileSelect(evt, prev_id, avatar) {
+    if (!evt) // i.e. the argument is undefined or null
+      evt = window.event;
     var files = evt.target.files; // FileList object
-// Loop through the FileList and render image files as thumbnails.
+    // Loop through the FileList and render image files as thumbnails.
     for (var i = 0, f; f = files[i]; i++) {
-// Only process image files.
+
+        // Only process image files.
         if (!f.type.match('image.*')) {
             continue;
         }
+
         var reader = new FileReader();
-// Closure to capture the file information.
+
+        // Closure to capture the file information.
         reader.onload = (function (theFile) {
             return function (e) {
-// Render thumbnail.
-// var span = document.createElement('span');
-                var span = document.getElementById('prev');
-                span.innerHTML = ['<img src="', e.target.result,
+                // Render thumbnail.
+                // var span = document.createElement('span');
+                var span = document.getElementById(prev_id);
+                span.innerHTML = ['<img class="thumb" src="', e.target.result,
                     '" title="', escape(theFile.name), '"/>'].join('');
-                document.getElementById('prev').insertBefore(span, null);
+                document.getElementById(prev_id).insertBefore(span, null);
             };
         })(f);
-// Read in the image file as a data URL.
+
+        // Read in the image file as a data URL.
         reader.readAsDataURL(f);
+    }
+
+    if (avatar.length > 0) {
+     funct_avatar(avatar);
     }
 }
