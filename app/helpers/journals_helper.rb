e61cf755bf0634cd034b27dda537ede516d76b64
Sandro SPECCHIA <sandro@pecchia.info>
2013-06-28 23:16:43 +0200

    fast repli to issue

--- app/helpers/journals_helper.rb
+++ app/helpers/journals_helper.rb
@@ -31,7 +31,7 @@ module JournalsHelper
                                                 :title => l(:button_edit)) if editable
     end
     content << content_tag('div', links.join(' '), :class => 'contextual') unless links.empty?
-    content << textilizable(journal, :notes)
+    content << journal.notes
     css_classes = "wiki"
     css_classes << " editable" if editable
     content_tag('div', content, :id => "journal-#{journal.id}-notes", :class => css_classes)
