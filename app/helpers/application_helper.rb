97cf8e80ffcc7f0d29005ce899c440433f2926fd
Sandro SPECCHIA <sandro@pecchia.info>
2013-09-14 11:23:10 +0200

    fix

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1257,7 +1257,7 @@ module ApplicationHelper
     op = options[:only_path].present?
     op =   options[:only_path] == false
     pre = "http://#{Setting.host_name}/images/"
-    no_img = (op ? "#{pre}articoli/#{taglia.to_s}_art-no-image.jpg" : "/images/articoli/#{taglia.to_s}_art-no-image.jpg")
+    no_img = (op ? "#{pre}commons/#{taglia.to_s}_art-no-image.jpg" : "/images/commons/#{taglia.to_s}_art-no-image.jpg")
     if  !articolo.image_file_name.nil?
       if !articolo.image.file?
         return no_img
