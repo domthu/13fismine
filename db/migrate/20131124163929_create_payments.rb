51e949592ff920074a579b2bed4f41172f33aa26
unknown <spec@PC.(none)>
2013-12-07 18:13:11 +0100

    migration payments

--- db/migrate/20131124163929_create_payments.rb
+++ db/migrate/20131124163929_create_payments.rb
@@ -2,14 +2,17 @@ class CreatePayments < ActiveRecord::Migration
   def self.up
     create_table :payments do |t|
       t.text :pagamento
-
       t.timestamps
     end
     change_column :invoices, :user_id, :int, :null => true
+    add_column :invoices, :payment_id, :int, :default => 1
+    add_column :invoices, :footer, :text, :null => true
   end
 
   def self.down
     drop_table :payments
+    remove_column :invoices, :payment_id
+    remove_column :invoices, :footer
   end
 
 end
