4a5b2a8bad432ab99da011d836ac1f00493b5dc4
specchia <sandro.pecchia@gmail.com>
2014-01-30 18:54:25 +0100

    fix facebook share

---
+++ config/initializers/facebook_share.rb
@@ -0,0 +1,12 @@
+FacebookShare.default_facebook_share_options = {
+    :framework => :jquery,
+    :jquery_function => "$",
+
+    :app_id => "231579800299114",
+    :status => "false",
+    :cookie => "false",
+    :xfbml => "false",
+
+    :selector => '.fb_share',
+    :locale => "en_US"
+}
