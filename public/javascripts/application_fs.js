d4f21a40ea585d849a0be121d5bc95dbe00ea48f
THUAL Dominique <dom_thual@yahoo.fr>
2014-06-16 01:27:18 +0200

    another tts

--- public/javascripts/application_fs.js
+++ public/javascripts/application_fs.js
@@ -88,8 +88,11 @@ function setVisible(id, visible) {
 }
 
 function speak(divid) {
+  get_id(divid,'it','fm');
+  //testare Oddcast e Intranslator
+  //www.ispeech.org/text.to.speech?link=http%3A%2F%2Fwww.ispeech.org%2Ftext.to.speech%3Fvoice%3Deuritalianfemale%26action%3Dconvert%26speed%3D0%26text%3Ddire%2520qualcosa
   //var urltts = "http://tts-api.com/tts.mp3?q="; solo inglese
   //var urltts = "http://www.ispeech.org/text.to.speech?voice=euritalianfemale&action=convert&speed=0&text=";
-  var urltts = "https://www.yakitome.com/api/rest/tts?api_key=your_api_key&voice=Audrey&speed=5&text="
-  $("#divaudio").html("<audio  controls autoplay><source src=" + urltts + escape($("#" + divid).html()) + " type=audio/mpeg></audio>")
+  //var urltts = "https://www.yakitome.com/api/rest/tts?api_key=your_api_key&voice=Audrey&speed=5&text=";
+  //$("#divaudio").html("<audio controls autoplay><source src='" + urltts + escape($("#" + divid).html()) + "' type=audio/mpeg></audio>")
 }
